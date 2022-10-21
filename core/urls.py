fa657f625e5140544266ff570a521f15d5b16cf7
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2022-10-21 18:19:23 +0500

    url home

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from django.shortcuts import render
+from myapp.views import home
 
 def main(request):
     return HttpResponse("<h1>Hello world</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',main)
+    path('',home),
 ]
