dfe4f37d2fbf6a35300eada8bf75350329e2dfdf
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2022-10-29 17:09:01 +0500

    1

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 from django.shortcuts import render
-from myapp.views import add,get_company,get_products,get_RAM,get_products_by_color,get_products_by_price,get_products_by_img,get_products_by_name
+from myapp.views import add,get_company,get_products,get_RAM,get_products_by_color,get_products_by_price,get_products_by_img,get_products_by_name,companyes, get_oraliq_price
 
 def main(request):
     return HttpResponse("<h1>Hello world</h1>")
@@ -12,10 +12,12 @@ urlpatterns = [
     path('add/', add),
     path('get_products', get_products),
     # path('',home),
-    path('get_company/<str:company>', get_company),
+    path('get_company/<str:gre>/', get_company, name="company"),
     path("get_RAM/<str:RAM>", get_RAM),
     path('get_products_by_color/<str:color>', get_products_by_color),
     path('get_products_by_price/<str:price>', get_products_by_price),
     path('get_products/get_products_by_img/<str:img_url>', get_products_by_img),
     path('get_products_by_name/<str:name>', get_products_by_name),
+    path('companyes/', companyes),
+    path("get_oraliq_price/<str:price>", get_oraliq_price)
 ]
